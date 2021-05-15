38c04b8c7aa29a801f89a994a9becac5c45ad230
jrMarty <melvinmartin1231@gmail.com>
2021-05-15 20:08:59 +0530

    Added login compenent
    Added login component and removed extra code.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  title = "Becasue of God's grace. Thank God";
+
+  months = ["January", "Feburary", "March", "April", "May", 
+            "June", "July", "August", "September",
+            "October", "November", "December"];
+  isAvailable = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
